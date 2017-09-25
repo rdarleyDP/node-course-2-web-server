9209c059a31123daf8eb83aed974440af69ff439
Robin Darley <rdarley@developerprogram.com>
2017-09-25 10:35:24 -0500

    Create helper function for footer

--- app.js
+++ app.js
@@ -1,16 +1,26 @@
+// Require modules
 const express = require('express');
 const hbs = require('hbs');
 const fs = require('fs');
-var app = express();
 
+// Setup port for heroku and local 
 const port = process.env.PORT || 3000;
 
-hbs.registerPartials(__dirname + '/views/partials');
+// Initiate express app
+var app = express();
 
+// Register partials for handlebars
+hbs.registerPartials(__dirname + '/views/partials');
 app.set('view engine', 'hbs');
 
 app.use(express.static(__dirname + '/public'));
 
+// Setup a handlebars helper for the current year
+hbs.registerHelper('getCurrentYear', () => {
+    return new Date().getFullYear();
+});
+
+// Home route
 app.get('/', (req, res) => {
     var statementCollection = JSON.parse(fs.readFileSync('statements.json'));
     
@@ -20,12 +30,14 @@ app.get('/', (req, res) => {
     });
 });
 
+// Test route
 app.get('/test', (req, res) => {
     res.render('home.hbs', {
         pageTitle: 'Test page'
     });
 });
 
+// Tell express to listen on the chosen port
 app.listen(port, () => {
     console.log(`Server is up on port ${port}`);
 });
